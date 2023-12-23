55263d08beafb419ff49ef6ed201bdf828da5a0d
AbuAhamedRafi <108174719+AbuAhamedRafi@users.noreply.github.com>
2023-12-23 13:22:54 +0600

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-// Select the "Get Started" button by its ID
-const getStartedBtn = document.getElementById('getStartedBtn');
-
-// Add a click event listener to the button
-getStartedBtn.addEventListener('click', function() {
-    // Redirect to the second page (newpage.html)
-    window.location.href = 'cost.html';
-});
